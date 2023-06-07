67d7d09979bbb06d15b3d45fbafcfb3261f975e1
JanLinnenbrink <jan.linnenbrink@web.de>
2023-06-07 09:52:48 +0200

    file structure changed1

--- simulation_deBruin/R/install.R
+++
@@ -1,8 +0,0 @@
-# future.apply  globals  listenv  NNDM  parallelly
-# R.version # 4
-.libPaths("/home/j/jlinnenb/r_packages/")
-
-install.packages("globals", repos='http://cran.us.r-project.org')
-install.packages("sperrorest", repos="http://cran.us.r-project.org")
-install.packages("ranger", repos="http://cran.us.r-project.org")
-
